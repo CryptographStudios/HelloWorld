0a48f43d4b91c8adbe900a23c22f2286c5c8fdeb
Zer0SixX <w.d.hill.3@gmail.com>
2018-08-27 00:31:55 -0500

    Try again!

---
+++ HelloTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d467e3a-8843-4391-8e3e-4989f010e0cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloTest/TestSamples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloTest
+{
+    [TestClass]
+    public class TestSamples
+    {
+        [TestMethod]
+        public void SuccessTest()
+        {
+        }
+
+        [TestMethod]
+        public void FailTest()
+        {
+            int? nullable = null;
+            Assert.IsNotNull(nullable);
+        }
+    }
+}
